0967baa00baccd7cbb3c1d2316ef63f2f6c06099
LapisSea <toni.tube.you@gmail.com>
2018-01-21 20:40:24 +0100

    Automated commit - Sun 01/21/2018

--- src/com/lapissea/vulkanimpl/VkGpu.java
+++ src/com/lapissea/vulkanimpl/VkGpu.java
@@ -3,33 +3,56 @@ package com.lapissea.vulkanimpl;
 import com.lapissea.glfwwin.GlfwWindow;
 import com.lapissea.vulkanimpl.util.VkDestroyable;
 import com.lapissea.vulkanimpl.util.VkGpuCtx;
-import org.lwjgl.vulkan.VK10;
-import org.lwjgl.vulkan.VkAllocationCallbacks;
-import org.lwjgl.vulkan.VkPhysicalDevice;
-import org.lwjgl.vulkan.VkPhysicalDeviceMemoryProperties;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.*;
 
+import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.vulkan.VK10.*;
 
 public class VkGpu implements VkDestroyable, VkGpuCtx{
 	
 	private final GlfwWindow window;
 	
-	private      VkAllocationCallbacks allocator;
-	public final VkPhysicalDevice      physicalDevice;
+	private VkAllocationCallbacks allocator;
+	
+	private final VkPhysicalDevice physicalDevice;
+	private       VkDevice         logicalDevice;
+	
 	private VkPhysicalDeviceMemoryProperties memoryProperties;
+	private VkPhysicalDeviceFeatures         features;
+	
 	
 	public VkGpu(GlfwWindow window, VkPhysicalDevice physicalDevice){
 		this.window=window;
 		this.physicalDevice=physicalDevice;
+		
+		memoryProperties=VkPhysicalDeviceMemoryProperties.malloc();
+		vkGetPhysicalDeviceMemoryProperties(physicalDevice, memoryProperties);
+		
+		features=VkPhysicalDeviceFeatures.malloc();
+		vkGetPhysicalDeviceFeatures(physicalDevice, features);
 	}
 	
-	public VkAllocationCallbacks getAllocator(){
-		return allocator;
+	public void init(){
+		
+		try(MemoryStack stack=stackPush()){
+			VkDeviceCreateInfo info=VkDeviceCreateInfo.callocStack(stack);
+			
+			logicalDevice=Vk.createDevice(physicalDevice, info, stack.mallocPointer(1));
+		}
 	}
 	
 	@Override
 	public void destroy(){
-	
+		
+		if(logicalDevice!=null){
+			vkDestroyDevice(logicalDevice, allocator);
+			
+		}
+		
+		memoryProperties=null;
+		features=null;
+		logicalDevice=null;
 	}
 	
 	@Override
@@ -38,27 +61,29 @@ public class VkGpu implements VkDestroyable, VkGpuCtx{
 	}
 	
 	
-	public VkPhysicalDeviceMemoryProperties getMemoryProperties(){
-		if(memoryProperties==null){
-			memoryProperties=VkPhysicalDeviceMemoryProperties.calloc();
-			vkGetPhysicalDeviceMemoryProperties(physicalDevice, memoryProperties);
-		}
-		
-		return memoryProperties;
-	}
-	
 	public int findMemoryType(int typeBits, int properties){
-		VkPhysicalDeviceMemoryProperties deviceMemoryProperties=getMemoryProperties();
 		
 		int bits=typeBits;
 		for(int i=0;i<VK_MAX_MEMORY_TYPES;i++){
 			if((bits&1)==1){
-				if((deviceMemoryProperties.memoryTypes(i).propertyFlags()&properties)==properties){
+				if((memoryProperties.memoryTypes(i).propertyFlags()&properties)==properties){
 					return i;
 				}
 			}
 			bits>>=1;
 		}
 		return -1;
+	}
+	
+	public VkPhysicalDeviceFeatures getFeatures(){
+		return features;
+	}
+	
+	public VkAllocationCallbacks getAllocator(){
+		return allocator;
+	}
+	
+	public VkPhysicalDeviceMemoryProperties getMemoryProperties(){
+		return memoryProperties;
 	}
 }
